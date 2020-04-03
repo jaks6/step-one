d572f98e58eb19e46d07f5eedfb6c433a1b27014
Jakob Mass <jaks@ut.ee>
2020-04-03 12:04:28 +0300

    Established default values for CPU speed configuration.
    Default values are:
    * No. of CPUs: 1
    * CPU speed: 10000 DMIPS

--- sim-flowable/src/main/java/ee/mass/epm/sim/CpuConf.java
+++ sim-flowable/src/main/java/ee/mass/epm/sim/CpuConf.java
@@ -2,6 +2,8 @@ package ee.mass.epm.sim;
 
 public class CpuConf {
 
+    private static final int DEFAULT_CPU_SPEED = 10000;
+    private static final int DEFAULT_NO_OF_CPUS = 1;
     int cpuSpeed;
     int noOfCpus;
 
@@ -24,5 +26,10 @@ public class CpuConf {
     public CpuConf(int cpuSpeed, int noOfCpus) {
         this.cpuSpeed = cpuSpeed;
         this.noOfCpus = noOfCpus;
+    }
+
+    public CpuConf () {
+        this.cpuSpeed = DEFAULT_CPU_SPEED;
+        this.noOfCpus = DEFAULT_NO_OF_CPUS;
     }
 }
