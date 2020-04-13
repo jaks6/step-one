e8d12ae9e8d214f96a9115ab81fe8eb0d2c204f4
Jakob Mass <jaks@ut.ee>
2020-04-13 17:49:08 +0300

    Add Process Modeler docker-compose project.
    Uses Postgres and Flowable Modeller with STEP-ONE-specific customizations.
    .war is provided as release asset

---
+++ step-one-modeler/modeler-webapp/wait-for-something.sh
@@ -0,0 +1,17 @@
+#!/bin/ash
+echo "Waiting for $3..."
+set -e
+
+host="$1"
+port="$2"
+description="$3"
+shift 3
+cmd="$@"
+
+until nc -z "$host" "$port"; do
+    echo "$description is unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "$description is up - executing command"
+exec $cmd
